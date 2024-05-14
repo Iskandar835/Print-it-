b7409aa3303e0f9da71910475c312ec677d713c2
Sacha Vandermoeten <sachadu835@gmail.com>
2024-05-14 16:21:47 +0200

    first commit

---
+++ assets/script.js
@@ -0,0 +1,43 @@
+const slides = [
+  {
+    image: "slide1.jpg",
+    tagLine: "Impressions tous formats <span>en boutique et en ligne</span>",
+  },
+  {
+    image: "slide2.jpg",
+    tagLine:
+      "Tirages haute définition grand format <span>pour vos bureaux et events</span>",
+  },
+  {
+    image: "slide3.jpg",
+    tagLine: "Grand choix de couleurs <span>de CMJN aux pantones</span>",
+  },
+  {
+    image: "slide4.png",
+    tagLine: "Autocollants <span>avec découpe laser sur mesure</span>",
+  },
+];
+
+const arrowBtn = document.querySelectorAll(".arrow");
+
+arrowBtn.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    const calcNextSlide = e.target.id === "next" ? 1 : -1;
+    const sliderImg = document.querySelector(".banner-img");
+    const allDots = document.querySelectorAll(".dot");
+    const slideDot = document.querySelector(".dot_selected");
+
+    let indexSituer = calcNextSlide + [...allDots].indexOf(slideDot);
+
+    if (indexSituer < 0) indexSituer = [...allDots].length - 1;
+    if (indexSituer >= [...allDots].length) indexSituer = 0;
+    allDots[indexSituer].classList.add("dot_selected");
+    slideDot.classList.remove("dot_selected");
+
+    sliderImg.setAttribute(
+      "src",
+      "assets/images/slideshow/" + slides[indexSituer].image
+    );
+    document.querySelector("#banner p").innerHTML = slides[indexSituer].tagLine;
+  });
+});
